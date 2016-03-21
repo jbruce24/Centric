7f2bc52c2c7264bd4114dddc2967714a9a26e38b
Jared Bruce <jared.bruce24@gmail.com>
2016-03-21 10:48:32 -0400

    Updated Site

---
+++ Account/Register.aspx.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.UI;
+using WebSite3;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
+
+public partial class Account_Register : Page
+{
+    protected void CreateUser_Click(object sender, EventArgs e)
+    {
+        var manager = new UserManager();
+        var user = new ApplicationUser() { UserName = UserName.Text };
+        IdentityResult result = manager.Create(user, Password.Text);
+        if (result.Succeeded)
+        {
+            // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+            //string code = manager.GenerateEmailConfirmationToken(user.Id);
+            //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
+            //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+
+            //signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+            string cs = ConfigurationManager.ConnectionStrings["CSUserData"].ToString();
+            SqlConnection conUsers = new SqlConnection(cs);
+            string sql = "Insert INTO [CentricUsers] ([userId], [firstName], [lastName], [Twitter], [HireDate], [Title], [Department]) Values (@userID, @firstName, @lastName, @twitter, @HireDate, @title, @department)";
+            SqlCommand comNewUse = new SqlCommand(sql, conUsers);
+            comNewUse.Parameters.Add("@userID", System.Data.SqlDbType.NVarChar, 50).Value = UserName.Text;
+            comNewUse.Parameters.Add("@lastName", System.Data.SqlDbType.NVarChar, 50).Value = lastTxt.Text;
+            comNewUse.Parameters.Add("@firstName", System.Data.SqlDbType.NVarChar, 50).Value = firstTxt.Text;
+            comNewUse.Parameters.Add("@twitter", System.Data.SqlDbType.NVarChar, 50).Value = Twitter.Text;
+            comNewUse.Parameters.Add("@HireDate", System.Data.SqlDbType.NVarChar, 50).Value = HireDate.Text;
+            comNewUse.Parameters.Add("@title", System.Data.SqlDbType.NVarChar, 50).Value = title.Text;
+            comNewUse.Parameters.Add("@department", System.Data.SqlDbType.NVarChar, 50).Value = department.Text;
+            try
+            {
+                conUsers.Open();
+                comNewUse.ExecuteNonQuery();
+            }
+
+            catch (Exception ex)
+            {
+                lblOutput.Text = "Sorry, unable to store user data.  Standard exception message is: " + ex.Message;
+                return;
+            }
+            finally
+            {
+                conUsers.Close();
+            }
+            IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+        }
+        else
+        {
+            ErrorMessage.Text = result.Errors.FirstOrDefault();
+        }
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Getting started</h2>
+           <div class="row">
+        <div class="col-md-4">
+            <h2>Twitter</h2>
+            <p>
+                <a class="twitter-timeline" href="https://twitter.com/centric" data-widget-id="707751014024273920">Tweets by @centric</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+        </div>
+        <div class="col-md-4">
+            <h2>Profile</h2>
+            <p>&nbsp;</p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Linkedin</h2>
+            <p>
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/CompanyInsider" data-id="11878"></script>
+          
+            <p>
+                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+</asp:Content>
