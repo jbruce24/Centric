dfca6457df8bad05478a2255ae66b2637af793e2
jt109912 <jt109912@ohio.edu>
2016-04-22 11:21:23 -0400

    Added files via upload

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
 
 public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        MaintainScrollPositionOnPostBack = true;
+        txtUser.Text = HttpContext.Current.User.Identity.Name;
+    }
 
+    protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+    {
+
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        //string strconnection = ConfigurationManager.ConnectionStrings["CSUserData"].ToString();
+        //SqlConnection myConnection = new SqlConnection(strconnection);
+        //string strSql = "UPDATE CENTRICUSERS SET POINTS = POINTS + 1 WHERE UserID = '" + DetailsView1.DataKey[0].ToString() + "'";
+        //SqlCommand myCommand = new SqlCommand(strSql, myConnection);
+
+        //myConnection.Open();
+        //int intCnt = myCommand.ExecuteNonQuery();
+        //myConnection.Close();
+
+        //DetailsView1.DataBind();
+        
     }
 }
